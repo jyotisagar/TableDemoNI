426ccda6c71a05c5cb2ef50179a0bc880c25363f
jyoti <jsagarbit@gmail.com>
2019-06-13 00:07:34 +0530

    dynamic column

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,25 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  tableColumn = [];
   tableData=[
    [    
-    {key:'column1',value:'val11'},
-    {key:'column2',value:'val12'}
+    {key:'First Name',value:'Jyoti'},
+    {key:'Last Name',value:'Sagar'},
+    {key:'Address',value:'PS'},
+    {key:'Mobile',value:'123'}
    ],
    [    
-    {key:'column1',value:'val21'},
-    {key:'column2',value:'val22'}
+    {key:'First Name',value:'Gaurav'},
+    {key:'Last Name',value:'Gupta'},
+    {key:'Address',value:'Hj'},
+    {key:'Mobile',value:'456'}
    ],
    [    
-    {key:'column1',value:'val31'},
-    {key:'column2',value:'val32'}
+    {key:'First Name',value:'Manish'},
+    {key:'Last Name',value:'Kumar'},
+    {key:'Address',value:'Rt'},
+    {key:'Mobile',value:'789'}
    ]
   ]
-  tableColumn = ['column1','column2']
   constructor() { }
 
   ngOnInit() {
+   
+    for (var i = 0; i < 1; i++) {     
+      for(var j in this.tableData[0]){
+        if (this.tableColumn.indexOf(j) === -1) {
+          this.tableColumn.push(this.tableData[i][j].key)
+        }  
+      }
+    }
   }
-  
-
 }
